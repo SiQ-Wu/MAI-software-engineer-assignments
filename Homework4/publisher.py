352d1b30822d641f8f25b1fae1ce2bce2c2d0693
Siqi Wu <56854944+SiQ-Wu@users.noreply.github.com>
2024-05-01 15:30:26 +0300

    Update publisher.py

--- Homework4/publisher.py
+++ Homework4/publisher.py
@@ -15,6 +15,9 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect, result code {0}".format(str(rc)))
         
+def on_publish(client, userdata, mid):
+    print('Sending data')
+        
 def mqtt_pub(vx0, vy0, sim_t):
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -24,20 +27,23 @@ def mqtt_pub(vx0, vy0, sim_t):
     
     t = np.linspace(0,sim_t,sim_t+1)
     vx = vx0 - g * t
-    vy = vy0
+    vy = vy0 + t * 0
     x = vx0 * t -  0.5 * g * t ** 2
     y = vy0 * t
     
     for i in range(len(t)):
-        data = str(t[i]) + ' ' + str(format(x[i], '.1f')) + ' ' + str(format(y[i], '.1f')) + \
-        ' ' + str(format(vx[i], '.1f')) + ' ' + str(vy[i])
+
+        data = str(t[i]) + ' ' + str(x[i]) + ' ' + str(y[i]) + ' ' + str(vx[i]) + ' ' + str(vy[i])
         client.publish(topic=topic, payload=data, retain=False)
         time.sleep(1)
-    
+        
 
 if __name__ == '__main__':
+    '''
     vx, vy, t = input('Input Vertical speed, Horizontal speed and Simulation time, split by space').split(' ')
     vx = int(vx)
     vy = int(vy)
     t = int(t)
-    mqtt_pub(vx, vy, t)
+    
+    '''
+    mqtt_pub(100, 15, 15)
